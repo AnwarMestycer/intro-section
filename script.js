86522f1972d26bc31202eaff9a805da099fe6727
AnwarMestycer <anwarmestycer0@.com>
2024-04-02 13:46:34 +0100

    mobile screen is ready

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 function changeLayout() {
-  const image = document.getElementById("image");
-  const navbar = document.getElementById("navbar");
-  const auth = document.getElementById("auth");
-  const header = document.getElementById("header");
-  
-  const imageUrl = "/images/icon-menu.svg";
-  let navImage = document.createElement("img");
-  navImage.src = imageUrl;
+  var sidebar = document.getElementById("sidebar");
+  var image = document.getElementById("image");
+  var menu = document.getElementById("menu");
+  var dropdownI = document.getElementById("dropdownI");
+  var dropdownII = document.getElementById("dropdownII");
 
   if (window.matchMedia("(max-width: 600px)").matches) {
     image.src = "/images/image-hero-mobile.png";
-    navbar.classList.add("hidden");
-    header.replaceChild(navImage, auth);
+    menu.classList.remove("hidden");
   } else {
     image.src = "/images/image-hero-desktop.png";
+    menu.classList.add("hidden");
   }
+}
+
+function toggleSidebar() {
+  sidebar.classList.remove("-right-[250px]");
+  sidebar.classList.add("right-[0]");
+  sidebar.classList.add("top-0");
 }
 
 window.onload = changeLayout;
